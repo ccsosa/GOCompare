e306e01762717771d4eeb74a9e7f2f90d0d804fd
DESKTOP-VAILM5K\user <chrystiam.sossa@gmail.com>
2025-05-21 09:41:57 -0500

    no message

--- R/GOCompare-package.R
+++ R/GOCompare-package.R
@@ -1,19 +1,12 @@
-#' GOCompare is a an R package used to compare a GO terms list between
-#'  two species
-#' @name GOCompare-package
-#' @aliases GOCompare
-#' @aliases GOCompare-package
-#' @docType package
-#' @title GOCompare: An R package to compare GO terms of gene lists (categories)  and their orthologs
+#' GOCompare: An R package to compare GO terms of gene lists (categories) and their orthologs
+#'
+#' GOCompare is an R package used to compare Gene Ontology (GO) term enrichment results between two species.
+#' It facilitates comparative functional genomics by allowing researchers to analyze similarities and differences
+#' in enriched GO categories between orthologous gene sets.
+#'
+#' Version: 1.0.2.1
+#' License: GPL-3
+#' Date: 2022-12-02
+#'
 #' @keywords package
-#'
-#' @details \tabular{ll}{
-#' Package: \tab GOCompare\cr
-#' Type: \tab Package\cr
-#' Version: \tab 1.0.2.1\cr
-#' Date: \tab 2022-12-02\cr
-#' License: \tab GPL-3\cr
-#' }
-#'
-
-NULL
+"_PACKAGE"
